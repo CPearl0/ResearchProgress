13739bad47e5c86e78e4b1cb0d874652f2c71155
CPearl0 <1269596929@qq.com>
2024-06-09 11:11:55 +0800

    rename

--- src/main/java/com/cpearl/researchprogress/compat/kjs/ResearchProgressPlugin.java
+++ src/main/java/com/cpearl/researchprogress/compat/kjs/ResearchProgressPlugin.java
@@ -3,7 +3,7 @@ package com.cpearl.researchprogress.compat.kjs;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.script.BindingsEvent;
 
-public class GamePhasePlugin extends KubeJSPlugin {
+public class ResearchProgressPlugin extends KubeJSPlugin {
     @Override
     public void registerBindings(BindingsEvent event) {
         event.add("ResearchProgress", new ResearchProgressKubeJSBindings());
